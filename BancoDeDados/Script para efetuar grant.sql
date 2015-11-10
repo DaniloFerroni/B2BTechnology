b22e6c4947715dcf7786960440d325ce4c9407cc
DaniloFerroni <danilo_ferroni09@hotmail.com>
2015-11-10 21:09:04 -0200

    Removendo arquivos não utilizados

--- BancoDeDados/Script para efetuar grant.sql
+++
@@ -1,7 +0,0 @@
-USE B2BSolution
-GO
-
-SELECT 'GRANT EXECUTE ON ' + NAME + ' TO b2bsolution' + CHAR(13) + CHAR(10) + 'GO' + CHAR(13) + CHAR(10) + CHAR(13) + CHAR(10)
-  FROM SYS.objects 
- WHERE TYPE = 'P' AND 
-	   name LIKE '%SPR%'
